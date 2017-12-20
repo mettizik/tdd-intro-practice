afd588bc2fc3ec08f16a363a70f2623b06486180
gamapat <nklyoma@gmail.com>
2017-12-21 00:29:12 +0200

    Fixed build. Added first test and built it.

--- tdd_intro/homework/01_bob/01_bob.pro
+++ tdd_intro/homework/01_bob/01_bob.pro
@@ -5,3 +5,5 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+SOURCES += \
+    bob.cpp
---
+++ tdd_intro/homework/01_bob/bob.cpp
@@ -0,0 +1,25 @@
+/*
+Bob is a lackadaisical teenager. In conversation, his responses are very limited.
+
+Bob answers 'Sure.' if you ask him a question. (? in the end)
+
+He answers 'Whoa, chill out!' if you yell at him. (! in the end)
+
+He says 'Fine. Be that way!' if you address him without actually saying anything.
+
+He answers 'Whatever.' to anything else.
+
+Implement Bob!
+*/
+#include <gtest/gtest.h>
+#include <string>
+
+std::string GetBobsAnswer(const std::string& request)
+{
+    return "";
+}
+
+TEST(GetBobsAnswer, GetBobsAnswer_Returns_Fine_On_Empty_Request)
+{
+    EXPECT_EQ("Fine. Be that way!", GetBobsAnswer(""));
+}
