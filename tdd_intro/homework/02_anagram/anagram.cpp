beead2df8749425a8e07c7ebdcf93878faae2c9c
gamapat <nklyoma@gmail.com>
2017-12-21 00:49:07 +0200

    Built second task. Created test and function.

---
+++ tdd_intro/homework/02_anagram/anagram.cpp
@@ -0,0 +1,21 @@
+/*
+Given a word and a list of possible anagrams, select the correct sublist.
+
+Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+*/
+
+#include <gtest/gtest.h>
+#include <string>
+#include <list>
+
+using ListOfStrings = std::list<std::string>;
+
+ListOfStrings GetAnnagrams(const std::string& word, const ListOfStrings& list)
+{
+    return ListOfStrings{""};
+}
+
+TEST(GetAnnagrams, GetAnnagrams_returns_empty_list_on_empty_input)
+{
+    EXPECT_EQ(ListOfStrings{}, GetAnnagrams("", {}));
+}
