b36ca49eab9d499c60f5257789376c8eed47e505
Volodymyr.Palii <palii.volodymyr@apriorit.com>
2017-12-21 14:20:13 +0200

    Solved problem 03_WordWrap

--- tdd_intro/homework/03_word_wrapp/03_word_wrapp.cpp
+++ tdd_intro/homework/03_word_wrapp/03_word_wrapp.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <numeric>
 
 using StringList_vt = std::vector<std::string>;
 
@@ -8,13 +9,19 @@ StringList_vt WordWrap(const std::string& input, size_t sizeLimit)
     StringList_vt result;
     size_t size = input.size();
     size_t offset = 0;
-    while (size > 0)
+    while (size > sizeLimit)
     {
-        const size_t segmentSize = std::min(size, sizeLimit);
-        result.push_back(input.substr(offset, segmentSize));
-        offset += segmentSize;
-        size -= segmentSize;
+        std::string segment = input.substr(offset, sizeLimit);
+        const size_t lastSpacePosition = segment.find_last_of(' ');
+        if (lastSpacePosition != std::string::npos)
+        {
+            segment.resize(lastSpacePosition + 1);
+        }
+        result.push_back(segment);
+        offset += segment.size();
+        size -= segment.size();
     }
+    result.push_back(input.substr(offset, size));
     return result;
 }
 
@@ -33,11 +40,36 @@ TEST(WordWrap, WordWrap_returns_list_with_all_segments_of_limit_size_except_last
     ASSERT_GT(inputString.size(), sizeLimit);
     const StringList_vt wrappedList = WordWrap(inputString, sizeLimit);
 
-    const size_t lastSegmentSize = inputString.size() % sizeLimit;
-    const size_t segmentCount = inputString.size() / sizeLimit + (lastSegmentSize > 0 ? 1 : 0);
+    const size_t segmentCount = wrappedList.size();
     for (size_t i = 0; i < segmentCount - 1; ++i)
     {
         EXPECT_EQ(wrappedList[i].size(), sizeLimit);
     }
-    EXPECT_EQ(wrappedList[segmentCount - 1].size(), lastSegmentSize);
+    EXPECT_LE(wrappedList[segmentCount - 1].size(), sizeLimit);
+}
+
+TEST(WordWrap, WordWrap_returns_list_with_all_segments_except_last_ending_with_spaces)
+{
+    const std::string inputString{"Very long test string with spaces. Very long test string with spaces."};
+    const size_t sizeLimit = 17;
+    ASSERT_GT(inputString.size(), sizeLimit);
+    const StringList_vt wrappedList = WordWrap(inputString, sizeLimit);
+
+    const size_t segmentCount = wrappedList.size();
+    for (size_t i = 0; i < segmentCount - 1; ++i)
+    {
+        EXPECT_LE(wrappedList[i].size(), sizeLimit);
+        EXPECT_THAT(wrappedList[i], testing::EndsWith(" "));
+    }
+}
+
+TEST(WordWrap, WordWrap_returns_list_what_after_concatenation_equals_to_input)
+{
+    const std::string inputString{"Very long test string with spaces. Very long test string with spaces."};
+    const size_t sizeLimit = 17;
+    ASSERT_GT(inputString.size(), sizeLimit);
+    const StringList_vt wrappedList = WordWrap(inputString, sizeLimit);
+
+    std::string concatenatedSegments = std::accumulate(wrappedList.begin(), wrappedList.end(), std::string{});
+    EXPECT_EQ(concatenatedSegments, inputString);
 }
