7baa3e82007dae3ee077fc0d655afe8490fe68d4
gamapat <nklyoma@gmail.com>
2017-12-24 12:37:02 +0200

    Made test for one character string green

--- tdd_intro/homework/05_trinary_numbers/test.cpp
+++ tdd_intro/homework/05_trinary_numbers/test.cpp
@@ -23,6 +23,18 @@ If your language provides a method in the standard library to perform the conver
 
 uint64_t ConvertFromTrinaryNumber(const std::string& number)
 {
+    if (number == "0")
+    {
+        return 0;
+    }
+    else if (number == "1")
+    {
+        return 1;
+    }
+    else if (number == "2")
+    {
+        return 2;
+    }
     return 0;
 }
 
