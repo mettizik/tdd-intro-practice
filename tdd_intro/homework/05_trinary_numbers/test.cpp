e91989c0a1bfd0edeb62292d8ce3735c90fde119
gamapat <nklyoma@gmail.com>
2017-12-24 12:26:34 +0200

    Prepared clean project

--- tdd_intro/homework/05_trinary_numbers/test.cpp
+++ tdd_intro/homework/05_trinary_numbers/test.cpp
@@ -1,14 +1,3 @@
-/*
-Given a phrase, count the occurrences of each word in that phrase.
-
-For example for the input "olly olly in come free"
-
-olly: 2
-in: 1
-come: 1
-free: 1
-
-*/
 
 #include <gtest/gtest.h>
 
