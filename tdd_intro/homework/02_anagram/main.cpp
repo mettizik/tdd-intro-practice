4762bc2a04376151817e6bfc7bd2c009c45c881a
Vlad Andreev <vladislavandr@gmail.com>
2017-12-28 11:36:13 +0200

    test1

--- tdd_intro/homework/02_anagram/main.cpp
+++ tdd_intro/homework/02_anagram/main.cpp
@@ -3,3 +3,18 @@
 //Given "listen" and a list of candidates like "enlists" "google" "inlets" "banana"
 //the program should return a list containing "inlets".
 
+#include <gtest/gtest.h>
+#include <vector>
+#include <string>
+
+using WordsList = std::vector<std::string>;
+
+WordsList GetAnagrams(std::string word, WordsList candidates)
+{
+    return {};
+}
+
+TEST(GetAnagrams, take_empty_list_returns_empty)
+{
+    EXPECT_EQ(WordsList{}, GetAnagrams("", {}));
+}
