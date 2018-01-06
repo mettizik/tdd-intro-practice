46930da976de079a0747edcf51c30c569ec584e7
prepodobny <prepa@ukr.net>
2018-01-06 17:36:49 +0200

    add first digit

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -1,14 +1,26 @@
 #include <gtest/gtest.h>
 
-using OCRData = std::vector<std::vector<char>>;
+using OCRData = std::vector<std::string>;
 
 
-std::string OCR2StringDigits(const OCRData& /*input*/)
+std::string OCR2StringDigits(const OCRData& input)
 {
-    return "";
+    if (input.empty())
+    {
+        return "";
+    }
+    return "1";
 }
 
-TEST(OCR2StringDigits, OCR2StringDigits)
+TEST(OCR2StringDigits, OCR2StringDigits_Check_Empty)
 {
     EXPECT_EQ("", OCR2StringDigits(OCRData()));
+}
+
+TEST(OCR2StringDigits, OCR2StringDigits_Take_One_OCR_Return_1)
+{
+    OCRData data = {{"   "},
+                    {"  |"},
+                    {"  |"}};
+    EXPECT_EQ("1", OCR2StringDigits(OCRData(data)));
 }
