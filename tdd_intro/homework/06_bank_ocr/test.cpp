05de0ebdd6a66fef20aa8a1129cace3a44e36128
gamapat <nklyoma@gmail.com>
2018-01-08 21:30:53 +0200

    Added tests list

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -1,2 +1,21 @@
 #include <gtest/gtest.h>
+#include <string>
+#include <vector>
+/*User Story 1
 
+The following format is created by the machine:
+    _  _     _  _  _  _  _
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+
+Each entry is 3 lines long, and each line has 27 characters. 3 lines of each entry contain an account number written using pipes and underscores.
+
+Each account number should have 9 digits, all of which should be in the range 0-9. A normal file contains around 500 entries.
+
+Write a program that can take this file and parse it into actual account numbers.*/
+
+/*
+ * Test list:
+ * 1. Parse one digit number (0-9 - 10 separate tests)
+ * 2. Parse multiple digits number (10, 234, 123456789)
+*/
