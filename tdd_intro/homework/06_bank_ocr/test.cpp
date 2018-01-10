792ba02d922c829a12e751311a62a9475f34a10b
Volodymyr Palii <palii.volodymyr@apriorit.com>
2018-01-10 20:27:47 +0200

    Task 5: added parsing for digits four-nine

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -14,6 +14,30 @@ int RecognizeDigit(const std::string& line1, const std::string& line2, const std
     {
         return 3;
     }
+    else if (line1 == "   " && line2 == "|_|" && line3 == "  |")
+    {
+        return 4;
+    }
+    else if (line1 == " _ " && line2 == "|_ " && line3 == " _|")
+    {
+        return 5;
+    }
+    else if (line1 == " _ " && line2 == "|_ " && line3 == "|_|")
+    {
+        return 6;
+    }
+    else if (line1 == " _ " && line2 == "  |" && line3 == "  |")
+    {
+        return 7;
+    }
+    else if (line1 == " _ " && line2 == "|_|" && line3 == "|_|")
+    {
+        return 8;
+    }
+    else if (line1 == " _ " && line2 == "|_|" && line3 == " _|")
+    {
+        return 9;
+    }
     return 0;
 }
 
@@ -35,5 +59,47 @@ TEST(BankOcr, RecognizeDigit_recognizes_digit_three)
 {
     EXPECT_EQ(3, RecognizeDigit(" _ ",
                                 " _|",
+                                " _|"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_four)
+{
+    EXPECT_EQ(4, RecognizeDigit("   ",
+                                "|_|",
+                                "  |"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_five)
+{
+    EXPECT_EQ(5, RecognizeDigit(" _ ",
+                                "|_ ",
+                                " _|"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_six)
+{
+    EXPECT_EQ(6, RecognizeDigit(" _ ",
+                                "|_ ",
+                                "|_|"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_seven)
+{
+    EXPECT_EQ(7, RecognizeDigit(" _ ",
+                                "  |",
+                                "  |"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_eight)
+{
+    EXPECT_EQ(8, RecognizeDigit(" _ ",
+                                "|_|",
+                                "|_|"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_nine)
+{
+    EXPECT_EQ(9, RecognizeDigit(" _ ",
+                                "|_|",
                                 " _|"));
 }
