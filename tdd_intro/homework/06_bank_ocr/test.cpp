613c139228a5687527f6cc8195c6c412eba554f6
Volodymyr Palii <palii.volodymyr@apriorit.com>
2018-01-10 20:15:50 +0200

    Task 5, test 2: added parsing for digit two

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -2,12 +2,26 @@
 
 int RecognizeDigit(const std::string& line1, const std::string& line2, const std::string& line3)
 {
-    return 1;
+    if (line1 == "   " && line2 == "  |" && line3 == "  |")
+    {
+        return 1;
+    }
+    else if (line1 == " _ " && line2 == " _|" && line3 == "|_ ")
+    {
+        return 2;
+    }
 }
 
 TEST(BankOcr, RecognizeDigit_recognizes_digit_one)
 {
-    EXPECT_EQ(1, RecognizeDigit("  |",
+    EXPECT_EQ(1, RecognizeDigit("   ",
                                 "  |",
                                 "  |"));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_two)
+{
+    EXPECT_EQ(2, RecognizeDigit(" _ ",
+                                " _|",
+                                "|_ "));
 }
