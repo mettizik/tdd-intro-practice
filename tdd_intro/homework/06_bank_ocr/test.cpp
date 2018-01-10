8ad2d9c6d5cbd6bf13a00b659d38853eb69be96d
Volodymyr Palii <palii.volodymyr@apriorit.com>
2018-01-10 20:10:26 +0200

    Task 5, test 1: parsing for digit one

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -1,2 +1,13 @@
 #include <gtest/gtest.h>
 
+int RecognizeDigit(const std::string& line1, const std::string& line2, const std::string& line3)
+{
+    return 1;
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_one)
+{
+    EXPECT_EQ(1, RecognizeDigit("  |",
+                                "  |",
+                                "  |"));
+}
