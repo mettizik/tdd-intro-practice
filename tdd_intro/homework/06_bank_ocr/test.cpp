7a69e6c2aa98e9ef4f0ab63f627978fed5fdc0f3
Volodymyr Palii <palii.volodymyr@apriorit.com>
2018-01-10 20:18:47 +0200

    Task 5: added parsing for digit three

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -2,7 +2,7 @@
 
 int RecognizeDigit(const std::string& line1, const std::string& line2, const std::string& line3)
 {
-    if (line1 == "   " && line2 == "  |" && line3 == "  |")
+    if      (line1 == "   " && line2 == "  |" && line3 == "  |")
     {
         return 1;
     }
@@ -10,6 +10,11 @@ int RecognizeDigit(const std::string& line1, const std::string& line2, const std
     {
         return 2;
     }
+    else if (line1 == " _ " && line2 == " _|" && line3 == " _|")
+    {
+        return 3;
+    }
+    return 0;
 }
 
 TEST(BankOcr, RecognizeDigit_recognizes_digit_one)
@@ -24,4 +29,11 @@ TEST(BankOcr, RecognizeDigit_recognizes_digit_two)
     EXPECT_EQ(2, RecognizeDigit(" _ ",
                                 " _|",
                                 "|_ "));
+}
+
+TEST(BankOcr, RecognizeDigit_recognizes_digit_three)
+{
+    EXPECT_EQ(3, RecognizeDigit(" _ ",
+                                " _|",
+                                " _|"));
 }
