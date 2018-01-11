4751fa0b133996799cb8bb89537d1f36d548b7ea
Vlad Andreev <vladislavandr@gmail.com>
2018-01-11 12:56:35 +0200

    parsed several account numbers

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -91,89 +91,105 @@ namespace
     }
 }
 
-std::string ParseAccountNumbers(const Lines& lines)
+Lines ParseAccountNumbers(const Accounts& accounts)
 {
-    const auto digits = Split(lines);
-
-    std::string result;
-    for (const auto& digit : digits)
+    Lines lines;
+    for (const auto& accountLines : accounts)
     {
-        for (const auto& digitAndLines : s_digitToLines)
+        const auto accountDigits = Split(accountLines);
+
+        std::string accountNumber;
+        for (const auto& digit : accountDigits)
         {
-            if (digitAndLines.second == digit)
+            for (const auto& digitAndLines : s_digitToLines)
             {
-                result += std::to_string(digitAndLines.first);
-                break;
+                if (digitAndLines.second == digit)
+                {
+                    accountNumber += std::to_string(digitAndLines.first);
+                    break;
+                }
             }
         }
-    }
 
-    return result;
+        lines.push_back(accountNumber);
+    }
+    return lines;
 }
 
 TEST(ParseAccountNumbers, Take_zero_Returns_0)
 {
-    EXPECT_EQ("0", ParseAccountNumbers(s_digitToLines[0]));
+    EXPECT_EQ(Lines({"0"}), ParseAccountNumbers({s_digitToLines[0]}));
 }
 
 TEST(ParseAccountNumbers, Take_one_Returns_1)
 {
-    EXPECT_EQ("1", ParseAccountNumbers(s_digitToLines[1]));
+    EXPECT_EQ(Lines({"1"}), ParseAccountNumbers({s_digitToLines[1]}));
 }
 
 TEST(ParseAccountNumbers, Take_two_Returns_2)
 {
-    EXPECT_EQ("2", ParseAccountNumbers(s_digitToLines[2]));
+    EXPECT_EQ(Lines({"2"}), ParseAccountNumbers({s_digitToLines[2]}));
 }
 
 TEST(ParseAccountNumbers, Take_three_Returns_3)
 {
-    EXPECT_EQ("3", ParseAccountNumbers(s_digitToLines[3]));
+    EXPECT_EQ(Lines({"3"}), ParseAccountNumbers({s_digitToLines[3]}));
 }
 
 TEST(ParseAccountNumbers, Take_four_Returns_4)
 {
-    EXPECT_EQ("4", ParseAccountNumbers(s_digitToLines[4]));
+    EXPECT_EQ(Lines({"4"}), ParseAccountNumbers({s_digitToLines[4]}));
 }
 
 TEST(ParseAccountNumbers, Take_five_Returns_5)
 {
-    EXPECT_EQ("5", ParseAccountNumbers(s_digitToLines[5]));
+    EXPECT_EQ(Lines({"5"}), ParseAccountNumbers({s_digitToLines[5]}));
 }
 
 TEST(ParseAccountNumbers, Take_six_Returns_6)
 {
-    EXPECT_EQ("6", ParseAccountNumbers(s_digitToLines[6]));
+    EXPECT_EQ(Lines({"6"}), ParseAccountNumbers({s_digitToLines[6]}));
 }
 
 TEST(ParseAccountNumbers, Take_seven_Returns_7)
 {
-    EXPECT_EQ("7", ParseAccountNumbers(s_digitToLines[7]));
+    EXPECT_EQ(Lines({"7"}), ParseAccountNumbers({s_digitToLines[7]}));
 }
 
 TEST(ParseAccountNumbers, Take_eight_Returns_8)
 {
-    EXPECT_EQ("8", ParseAccountNumbers(s_digitToLines[8]));
+    EXPECT_EQ(Lines({"8"}), ParseAccountNumbers({s_digitToLines[8]}));
 }
 
 TEST(ParseAccountNumbers, Take_nine_Returns_9)
 {
-    EXPECT_EQ("9", ParseAccountNumbers(s_digitToLines[9]));
+    EXPECT_EQ(Lines({"9"}), ParseAccountNumbers({s_digitToLines[9]}));
 }
 
 TEST(ParseAccountNumbers, Take_2_digits_Returns_correct_string)
 {
-    EXPECT_EQ("10", ParseAccountNumbers(MakeAccountNumber("10")));
+    EXPECT_EQ(Lines({"10"}), ParseAccountNumbers({MakeAccountNumber("10")}));
 }
 
 TEST(ParseAccountNumbers, Take_3_digits_Returns_correct_string)
 {
-    EXPECT_EQ("105", ParseAccountNumbers(MakeAccountNumber("105")));
+    EXPECT_EQ(Lines({"105"}), ParseAccountNumbers({MakeAccountNumber("105")}));
 }
 
 TEST(ParseAccountNumbers, Take_account_number_Returns_correct_string)
 {
 
-    EXPECT_EQ("100500432", ParseAccountNumbers(MakeAccountNumber("100500432")));
+    EXPECT_EQ(Lines({"100500432"}), ParseAccountNumbers({MakeAccountNumber("100500432")}));
+}
+
+TEST(ParseAccountNumbers, Take_several_account_numbers_Returns_correct_strings)
+{
+    EXPECT_EQ(Lines({"111111111", "122222222", "333333333"}), ParseAccountNumbers(
+                    {
+                        MakeAccountNumber("111111111"),
+                        MakeAccountNumber("122222222"),
+                        MakeAccountNumber("333333333")
+                    }
+                  ));
 }
 
