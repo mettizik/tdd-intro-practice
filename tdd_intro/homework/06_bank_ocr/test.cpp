3a2516bddd899843e40acc99ab7260f52cf6eed8
Vlad Andreev <vladislavandr@gmail.com>
2018-01-11 10:58:49 +0200

    added tests list

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -1,2 +1,10 @@
 #include <gtest/gtest.h>
 
+/*
+ * Tests list:
+ * - parse a digit from a square 3x9
+ * - parse an account number (9 digits)
+ * - parse a wrong digit (not in the range 0-9)
+ * - parse several account numbers (500:)
+ * /
+
