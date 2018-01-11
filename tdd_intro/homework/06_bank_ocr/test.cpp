f755b41e078e0fd2816108a45f8f4d6f88686907
Valentyn Kyrychenko <bbjwwproj@gmail.com>
2018-01-11 09:02:46 +0200

    added test

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -5,3 +5,36 @@
 //2. 3 symbols parsing
 //3. 9 symbols parsing
 //4. work with file
+
+namespace
+{
+    using Lines = std::vector<std::string>;
+    const std::string one_top =    "   ";
+    const std::string one_mid =    "  |";
+    const std::string one_bottom = "  |";
+}
+
+int Parse(const Lines& lines)
+{
+    if (!lines.empty())
+    {
+        return 1;
+    }
+    return 0;
+}
+
+TEST(Bank, EmptyStrings)
+{
+    Lines lines;
+    EXPECT_EQ(Parse(lines), 0);
+}
+
+TEST(Bank, InvalidStingCount)
+{
+    Lines lines;
+    lines.push_back(one_top);
+    lines.push_back(one_mid);
+    lines.push_back(one_bottom);
+
+    EXPECT_EQ(Parse(lines), 1);
+}
