d9bc930d17163dc6310c51370758811816e83369
Vlad Andreev <vladislavandr@gmail.com>
2018-01-11 11:11:51 +0200

    test1

--- tdd_intro/homework/06_bank_ocr/test.cpp
+++ tdd_intro/homework/06_bank_ocr/test.cpp
@@ -6,5 +6,20 @@
  * - parse an account number (9 digits)
  * - parse a wrong digit (not in the range 0-9)
  * - parse several account numbers (500:)
- * /
+ */
 
+using Lines = std::vector<std::string>;
+
+std::string ParseAccountNumbers(const Lines& lines)
+{
+    return "1";
+}
+
+TEST(ParseAccountNumbers, Take_one_Returns_one)
+{
+    EXPECT_EQ("1", ParseAccountNumbers({
+                                           "   ",
+                                           "  |",
+                                           "  |"
+                                       }));
+}
