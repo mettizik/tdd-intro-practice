08a1cce29867bc5a31593d988753fa6445c70dea
prepodobny <prepa@ukr.net>
2018-01-12 10:33:49 +0200

    MockFileCopier

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -1,3 +1,11 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+TEST(FolderCopier, FolderCopier_CopyOneFile)
+{
+    MockFileCopier mock;
+
+    EXPECT_CALL(mock, Copy(_, _)).WillOnce(&ReturnsVoid);
+
+    mock.Copy("D:/", "E:/");
+}
