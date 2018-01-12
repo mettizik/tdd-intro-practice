0a916e778446f6b39a4e53978fcf9a4df5c2e8f0
prepodobny <prepa@ukr.net>
2018-01-12 10:41:13 +0200

    MockFileCopier

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -1,11 +1,24 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+class IFileCopier
+{
+public:
+    virtual ~IFileCopier() {}
+    virtual void Copy(const std::string& src, const std::string& dst) = 0;
+};
+
+class MockFileCopier: public IFileCopier
+{
+public:
+    MOCK_METHOD2(Copy, void(const std::string& src, const std::string& dst));
+};
+
 TEST(FolderCopier, FolderCopier_CopyOneFile)
 {
     MockFileCopier mock;
 
-    EXPECT_CALL(mock, Copy(_, _)).WillOnce(&ReturnsVoid);
+    EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).WillOnce(::testing::Return());
 
     mock.Copy("D:/", "E:/");
 }
