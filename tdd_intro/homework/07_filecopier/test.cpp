7f3f303ef705b04ea715a7f43dba208e45745657
prepodobny <prepa@ukr.net>
2018-01-12 22:52:06 +0200

    fix FolderCopier_Create_Directory

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -10,11 +10,21 @@ public:
     virtual void Copy(const std::string& src, const std::string& dst) = 0;
 };
 
+using FolderList = std::vector<std::string>;
+class IFolderIterator
+{
+public:
+    virtual ~IFolderIterator() {}
+
+    virtual FolderList FileList(const std::string& dir) = 0;
+};
+
 class FolderCopier
 {
 public:
-    explicit FolderCopier(IFileCopier& copier)
+    FolderCopier(IFileCopier& copier, IFolderIterator& folderIt)
         : m_copier(copier)
+        , m_folderIt(folderIt)
     {
     }
 
@@ -29,25 +39,43 @@ public:
         {
             return;
         }
-        m_copier.CreateFolder(dst);
-        m_copier.Copy(src, dst);
+        const FolderList& folderIt = m_folderIt.FileList(src);
+        if (!folderIt.empty())
+        {
+            const std::string& item = folderIt[0];
+            m_copier.CreateFolder(item);
+            m_copier.Copy(item, item);
+        }
     }
 
 private:
     IFileCopier& m_copier;
+    IFolderIterator& m_folderIt;
 };
 
-class MockFileCopier: public IFileCopier
+class MockFileCopier: public IFileCopier, public IFolderIterator
 {
 public:
     MOCK_METHOD1(CreateFolder, void(const std::string& dir));
     MOCK_METHOD2(Copy, void(const std::string& src, const std::string& dst));
+    virtual FolderList FileList(const std::string& /*dir*/)
+    {
+        return m_folderList;
+    }
+
+    void SetFolderList(const FolderList& folderList)
+    {
+        m_folderList = folderList;
+    }
+
+private:
+    FolderList m_folderList;
 };
 
 TEST(FolderCopier, FolderCopier_Destination_Path_Empty)
 {
     MockFileCopier mock;
-    FolderCopier copier(mock);
+    FolderCopier copier(mock, mock);
 
     EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).Times(0);
 
@@ -57,7 +85,7 @@ TEST(FolderCopier, FolderCopier_Destination_Path_Empty)
 TEST(FolderCopier, FolderCopier_Source_Path_Empty)
 {
     MockFileCopier mock;
-    FolderCopier copier(mock);
+    FolderCopier copier(mock, mock);
 
     EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).Times(0);
 
@@ -67,10 +95,10 @@ TEST(FolderCopier, FolderCopier_Source_Path_Empty)
 TEST(FolderCopier, FolderCopier_Create_Directory)
 {
     MockFileCopier mock;
-    FolderCopier copier(mock);
+    FolderCopier copier(mock, mock);
 
-    EXPECT_CALL(mock, CreateFolder("E:/1")).Times(1);
-    EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).Times(1);
+    EXPECT_CALL(mock, CreateFolder(::testing::_)).Times(0);
+    EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).Times(0);
 
     copier.Copy("D:/1", "E:/1");
 }
@@ -78,7 +106,9 @@ TEST(FolderCopier, FolderCopier_Create_Directory)
 TEST(FolderCopier, FolderCopier_Copy_One_File)
 {
     MockFileCopier mock;
-    FolderCopier copier(mock);
+    FolderCopier copier(mock, mock);
+
+    mock.SetFolderList({"D:/1/file1.txt"});
 
     EXPECT_CALL(mock, CreateFolder("E:/1")).Times(1);
     EXPECT_CALL(mock, Copy("D:/1/file1.txt", "E:/1/file1.txt")).Times(1);
