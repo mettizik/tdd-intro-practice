45122fb0a486bba5d6f56946392a2041b9a1f921
prepodobny <prepa@ukr.net>
2018-01-12 22:23:35 +0200

    add CreateFolder

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -5,6 +5,8 @@ class IFileCopier
 {
 public:
     virtual ~IFileCopier() {}
+
+    virtual void CreateFolder(const std::string& dir) = 0;
     virtual void Copy(const std::string& src, const std::string& dst) = 0;
 };
 
@@ -14,6 +16,11 @@ public:
     explicit FolderCopier(IFileCopier& copier)
         : m_copier(copier)
     {
+    }
+
+    virtual void CreateFolder(const std::string& dir)
+    {
+        m_copier.CreateFolder(dir);
     }
 
     virtual void Copy(const std::string& src, const std::string& dst)
@@ -32,6 +39,7 @@ private:
 class MockFileCopier: public IFileCopier
 {
 public:
+    MOCK_METHOD1(CreateFolder, void(const std::string& dir));
     MOCK_METHOD2(Copy, void(const std::string& src, const std::string& dst));
 };
 
@@ -55,12 +63,12 @@ TEST(FolderCopier, FolderCopier_Source_Path_Empty)
     copier.Copy("", "E:/1");
 }
 
-TEST(FolderCopier, FolderCopier_Copy_One_File)
+TEST(FolderCopier, FolderCopier_Create_Directory)
 {
     MockFileCopier mock;
     FolderCopier copier(mock);
 
-    EXPECT_CALL(mock, CreateFolder("E:/1",)).Times(1);
+    EXPECT_CALL(mock, CreateFolder("E:/1")).Times(1);
     EXPECT_CALL(mock, Copy(::testing::_, ::testing::_)).Times(1);
 
     copier.Copy("D:/1", "E:/1");
