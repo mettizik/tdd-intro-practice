c966bc1abd43d4eab246b808872f2ac04e5e78d7
Vlad Andreev <vladislavandr@gmail.com>
2018-01-18 13:29:06 +0200

    refactoring

--- tdd_intro/homework/07_filecopier/FileCopier.cpp
+++ tdd_intro/homework/07_filecopier/FileCopier.cpp
@@ -1,6 +1,14 @@
 #include "FileCopier.h"
 #include "IFileSystem.h"
 
+namespace
+{
+    bool IsSameLocation(const std::string& src, const std::string& dst)
+    {
+        return src.find(dst) != std::string::npos;
+    }
+}
+
 FileCopier::FileCopier(IFileSystem* fsys)
     : m_fsys(fsys)
 {
@@ -12,8 +20,7 @@ bool FileCopier::Copy(const std::string& src, const std::string& dst)
     {
         return false;
     }
-
-    if (src.find(dst) != std::string::npos)
+    if (IsSameLocation(src, dst))
     {
         return true;
     }
