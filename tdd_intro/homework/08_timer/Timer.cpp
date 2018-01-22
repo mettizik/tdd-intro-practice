521bd7c945d7061e7669a97bd426bc0839db5ab9
Nikita Kleymenov <kleymenov.nikita@apriorit.com>
2018-01-22 12:16:16 +0200

    Added initial test

--- tdd_intro/homework/08_timer/Timer.cpp
+++ tdd_intro/homework/08_timer/Timer.cpp
@@ -1,1 +1,16 @@
 #include "Timer.h"
+
+void Timer::Start()
+{
+
+}
+
+bool Timer::IsExpired() const
+{
+    return false;
+}
+
+Duration Timer::TimeLeft() const
+{
+    return Duration(0);
+}
--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -10,3 +10,20 @@ Followed by this specification:
     Timer allows to determine how much time left till period is expired
     Timer can be started again any time, no metter in what state it is
 */
+
+/*
+   Test list:
+   1. Start called and IsExpired returns false on non expired time
+   2. Start called and IsExpired returns true on expired time
+   3. Start called and TimeLeft returns duration beetwin maxTime and 1 on non expired time
+   4. Start called and TimeLeft returns 0 on expired time
+   5. Start called and after timer expires start called one more time and timer is no longer expired
+   6. Start called and before timer expires start called one more time and it is still not expired.
+*/
+
+TEST(Timer, TimerNotExpiresIfFlowedTimeIsLessThanTimerWasBoundTo)
+{
+    Timer timer;
+    timer.Start();
+    EXPECT_FALSE(timer.IsExpired());
+}
