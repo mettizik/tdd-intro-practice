558e797f8450787e884e0730388dd8e6cdfc7f15
Valentyn Kyrychenko <bbjwwproj@gmail.com>
2018-01-23 18:28:59 +0200

    wrote class with tests

--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -10,3 +10,45 @@ Followed by this specification:
     Timer allows to determine how much time left till period is expired
     Timer can be started again any time, no metter in what state it is
 */
+
+TEST(timer, checkIsExpiredWithoutStart)
+{
+    Timer t;
+    ASSERT_FALSE(t.IsExpired(100));
+}
+
+TEST(timer, checkIsExpired_beforeTime)
+{
+    Timer t;
+    t.Start(100, 10);
+    ASSERT_FALSE(t.IsExpired(109));
+}
+
+TEST(timer, checkIsExpired_afterTime)
+{
+    Timer t;
+    t.Start(100, 15);
+    ASSERT_TRUE(t.IsExpired(116));
+}
+
+TEST(timer, checkTimeLeft_beforeTime)
+{
+    Timer t;
+    t.Start(100, 15);
+    ASSERT_EQ(Duration(10), t.TimeLeft(105));
+}
+
+TEST(timer, checkTimeLeft_afterTime)
+{
+    Timer t;
+    t.Start(10, 15);
+    ASSERT_EQ(Duration(0), t.TimeLeft(105));
+}
+
+TEST(timer, checkTimerStart_reuse)
+{
+    Timer t;
+    t.Start(100, 15);
+    t.Start(110, 15);
+    ASSERT_EQ(Duration(1), t.TimeLeft(124));
+}
