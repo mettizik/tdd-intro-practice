90c86adec1cfc507d8bbeecc28e40f98d8c92a54
Vlad Andreev <vladislavandr@gmail.com>
2018-01-24 13:54:37 +0200

    test7-8, several Start calls

--- tdd_intro/homework/08_timer/test.cpp
+++ tdd_intro/homework/08_timer/test.cpp
@@ -6,8 +6,8 @@ Implement a timer class that implements ITimer interface defined in header file 
 
 Followed by this specification:
     + Timer allows to determine if specific period of time expired or not since Start was called last time
-    Timer allows to determine how much time left till period is expired
-    Timer can be started again any time, no metter in what state it is
+    + Timer allows to determine how much time left till period is expired
+    + Timer can be started again any time, no metter in what state it is
 */
 
 using sec = std::chrono::seconds;
@@ -50,4 +50,24 @@ TEST(Timer, TimeLeftReturnsZeroWhenTimerIsExpired)
     Timer t;
     t.Start(Clock::now());
     EXPECT_EQ(Duration{0}, t.TimeLeft(Clock::now()));
+}
+
+TEST(Timer, StartTimerTwiceAndExpireOnSecond)
+{
+    Timer t;
+    t.Start(Clock::now() + sec(2));
+    EXPECT_FALSE(t.IsExpired(Clock::now()));
+    t.Start(Clock::now() + sec(0));
+    EXPECT_TRUE(t.IsExpired(Clock::now()));
+}
+
+TEST(Timer, StartTimerThriceAndCheckTimeLeftCalls)
+{
+    Timer t;
+    t.Start(Clock::now() + sec(2));
+    EXPECT_EQ(sec(2), t.TimeLeft(Clock::now()));
+    t.Start(Clock::now() + sec(0));
+    EXPECT_EQ(Duration{0}, t.TimeLeft(Clock::now()));
+    t.Start(Clock::now() + sec(5));
+    EXPECT_EQ(sec(5), t.TimeLeft(Clock::now()));
 }
