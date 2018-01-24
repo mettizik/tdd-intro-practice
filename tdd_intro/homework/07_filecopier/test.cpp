3b0b04d6376a6349c69e4a345afd07f17cb48dcc
Volodymyr Palii <palii.volodymyr@apriorit.com>
2018-01-24 17:50:46 +0200

    Task 7: Source is absent test

--- tdd_intro/homework/07_filecopier/test.cpp
+++ tdd_intro/homework/07_filecopier/test.cpp
@@ -1,3 +1,27 @@
 #include <gtest/gtest.h>
-#include <gmock/gmock.h>
+
+#include <system_error>
+
+using PathType = std::string;
+
+class IFilesystem
+{
+public:
+};
+
+std::error_code CopyDirectory(IFilesystem* fs, const PathType& src, const PathType& dst)
+{
+    return std::make_error_code(std::errc::no_such_file_or_directory);
+}
+
+class MockFilesystem : public IFilesystem
+{
+public:
+};
+
+TEST(CopyDirectory, returns_nosuchfile_when_source_is_absent)
+{
+    MockFilesystem fs;
+    EXPECT_EQ(std::errc::no_such_file_or_directory, CopyDirectory(&fs, "/source", "/destination"));
+}
 
